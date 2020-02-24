e3631a7930e1437a3596d8bd7612f53c20c4d262
Vladimir <vm@piogroup.net>
2020-02-24 09:41:42 +0200

    adding

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ docker stop djecommerce_py
 docker rm djecommerce_py
 docker build -t djecommerce_py/py:1 .
 
-docker run -d --restart=always --network=djecommerce --ip=172.41.20.10 --name=djecommerce djecommerce_py/py:1
+docker run -d --restart=always --network=djecommerce --ip=172.41.20.10 --name=djecommerce_py djecommerce_py/py:1
 #docker exec --tty bakshieva_web python /app/manage.py makemigrations
 #docker exec --tty bakshieva_web python /app/manage.py migrate
 #docker exec --it bakshieva_web "/usr/local/bin/python /app/manage.py migrate"
